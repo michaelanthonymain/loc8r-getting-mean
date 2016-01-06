1cf00c9b1f2a74ac9eec3dee1b6d0ca848cc33bd
Michael Main <michaelanthonymain@gmail.com>
2016-01-05 21:48:29 -0600

    build location-info route and template

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,7 +5,7 @@ module.exports.homelist = function (req, res) {
 
 /* GET 'location info' page */
 module.exports.locationInfo = function (req, res) {
-    res.render('index', { title: 'Location Info' });
+    res.render('location-info', { title: 'Location Info' });
 };
 
 /* GET 'add review' page */
